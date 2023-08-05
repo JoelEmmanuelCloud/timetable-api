35604d37811f92854d14da227cbd491daec10a9b
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-05 05:16:08 +0100

    feat: setup authRouter

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import 'express-async-errors';
+import express from 'express';
+import connectDB from './db/connect';
+import dotenv from 'dotenv';
+import notFoundMiddleware from './middleware/not-found';
+import errorHandlerMiddleware from './middleware/error-handler';
+import authRouter from './routes/auth-router';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+app.use('/api/v1/auth', authRouter);
+const port = Number(process.env.PORT) || 5000;
+
+const MONGO_URL = process.env.MONGO_URL as string;
+
+const start = async () => {
+    try {
+        await connectDB(MONGO_URL);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`),
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
