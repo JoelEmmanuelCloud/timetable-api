87f928a0b1e40d9619e492bc359f6566f15ad66a
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-05 05:05:53 +0100

    feat: add auth controllers

---
+++ src/routes/auth-route.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const router = express.Router();
+import {login, register, logout} from '../controllers/auth-controller';
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/logout', logout);
+
+
+export default router;
+
+
+
