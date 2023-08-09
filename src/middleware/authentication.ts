8b79fdb2230ea3547812ef28840a194c4fd86b8d
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-09 12:46:55 +0100

    feat: remove commented code

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -26,22 +26,6 @@ const authenticateUser = async (req: ExtendedRequest, res: Response, next: NextF
 };
 
 
-// const authenticateUser = async (req: ExtendedRequest, res: Response, next: NextFunction): Promise<void> => {
-//   const token = req.signedCookies.token;
-
-//   if (!token) {
-//     throw new CustomError.UnauthenticatedError('Authentication Invalid');
-//   }
-
-//   try {
-//     const { name, userId, academyRole } = isTokenValid({ token });
-//     req.user = { name, userId, academyRole };
-//     next();
-//   } catch (error) {
-//     throw new CustomError.UnauthenticatedError('Authentication Invalid');
-//   }
-// };
-
 const authorizePermissions = (...roles: AcademyRole[]) => {
   return (req: ExtendedRequest, res: Response, next: NextFunction): void => {
     if (!req.user || !roles.includes(req.user.academyRole)) {
