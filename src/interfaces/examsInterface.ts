c0c39b52bcc59c4f8c8d3023f9897c9e22c9ae62
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-11 10:17:12 +0100

    feat: add exams interface

--- src/interfaces/examsInterface.ts
+++ src/interfaces/examsInterface.ts
@@ -4,22 +4,12 @@ export interface Exam {
     date: string;
     day: string;
     level: string;
-    morning?: {
-        course: string[] | string;
-        time: string;
-        venue: string;
-        supervisor: string;
-        lecturers: string[];
-        invigilators: string[];
-    };
-    afternoon?: {
-        course: string[] | string;
-        time: string;
-        venue: string;
-        supervisor: string;
-        lecturers: string[];
-        invigilators: string[];
-    };
+    course: string;
+    time: string;
+    venue: string;
+    supervisor: string[];
+    lecturers: string[];
+    invigilators: string[];
 }
 
 export interface ExamDocument extends Document, Exam {}
